06a66b30af31615cbd54303887207f40cb537204
Kapatych <onetruestory@ya.ru>
2020-05-10 15:20:32 +0300

    Added catalog page, refactored dateselector

--- src/components/dateselector/dateselector.js
+++ src/components/dateselector/dateselector.js
@@ -1,61 +1,54 @@
-//import Litepicker from 'litepicker';
-import Datepicker from 'air-datepicker';
+import datepicker from 'air-datepicker';
 
-// new Litepicker({
-//     element: document.querySelector('.datepicker__start'),
-//     elementEnd: document.querySelector('.datepicker__end'),
-//     lang: 'ru-RU',
-//     format: 'DD.MM.YYYY',
-//     singleMode: false,
-//     autoApply: false,
-//     autoRefresh: true,
-//     minDate: new Date().getTime(),
-//     useResetBtn: true,
-//     tooltipText: {
-//         'one': 'день',
-//         'few': 'дня',
-//         'many': 'дней'
-//     },
-//     buttonText: {
-//         apply: 'Применить',
-//         cancel: '',
-//         previousMonth: 'arrow_back',
-//         nextMonth: 'arrow_forward',
-//         reset: 'Очистить'
-//     },
-// });
-
-const datedropdown = document.querySelector('.dateselector');
-datedropdown.addEventListener('click', (event) => handleDatedropdownClick(event));
-
-function handleDatedropdownClick(event) {
-    if (event.target !== datedropdown) {
-        const datedropdownInput = datedropdown.querySelectorAll('.dateselector__input')[0];
-        datedropdownInput.focus();
-    }
+class Dateselector {
+  constructor(input, properties) {
+    $(input).datepicker({
+      ...properties,
+      range: true,
+      minDate: new Date(),
+      clearButton: true,
+      offset: 6,
+      prevHtml: 'arrow_back',
+      nextHtml: 'arrow_forward',
+      onShow(inst, animationCompleted) {
+        const buttonsContainer = inst.nav.$buttonsContainer;
+        if (!animationCompleted) {
+          // ONLY when apply-button doesn't exist
+          if (buttonsContainer.find('.datepicker--button-apply').html() === undefined) {
+            buttonsContainer.append('<span class="datepicker--button-apply">Применить</span>');
+            buttonsContainer.find('.datepicker--button-apply').click(function (event) {
+              inst.hide()
+            });
+          }
+        }
+      }
+    })
+  }
 }
 
-const firstInput = document.querySelector('.dateselector__start');
-const secondInput = document.querySelector('.dateselector__end');
+const dateselectors = document.querySelectorAll('.js-dateselector');
 
-$(firstInput).datepicker({
-    range: true,
-    minDate: new Date(),
-    clearButton: true,
-    offset: 6,
-    prevHtml: 'arrow_back',
-    nextHtml: 'arrow_forward',
-    onSelect(fd) {
-        $(firstInput).val(fd.split(',')[0]);
-        $(secondInput).val(fd.split(',')[1]);
-    },
-    onShow(dp, animationCompleted) {
-        const buttonsContainer = dp.nav.$buttonsContainer;
-        if (!animationCompleted) {
-            if (buttonsContainer.find('.datepicker--button-apply').html() === undefined) { /*ONLY when button don't existis*/
-                buttonsContainer.append('<span class="datepicker--button-apply">Применить</span>');
-                buttonsContainer.find('.datepicker--button-apply').click(function(event) { dp.hide() });
-            }
-        }
-    },
+dateselectors.forEach(dateselector => {
+  const firstInput = dateselector.querySelector('.dateselector__start');
+
+  if (firstInput) {
+    const secondInput = dateselector.querySelector('.dateselector__end');
+    secondInput.addEventListener('click', () => firstInput.focus());
+
+    new Dateselector(firstInput, {
+      onSelect(date) {
+        // Split formatted date between 2 inputs
+        $(firstInput).val(date.split(',')[0]);
+        $(secondInput).val(date.split(',')[1]);
+      }
+    });
+  } else {
+    const input = dateselector.querySelector('.dateselector__input');
+    new Dateselector(input, {
+      dateFormat: 'dd M',
+      multipleDatesSeparator: ' - ',
+    })
+  }
 });
+
+
